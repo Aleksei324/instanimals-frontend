ec582600ae87d05fc9ea3420c686764021477d24
ChrisDav03 <96332104+ChrisDav03@users.noreply.github.com>
2023-02-14 16:41:16 -0500

    Esto es una prueba
    Hola

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           <img src={"/react.svg"} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Probando aaaaaaaaaaaaaaaaaa</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
